28a30c15788fe0e8b218a3945f7b7aac1f661291
Your Name <you@example.com>
2018-01-30 19:00:03 +0800

    Modify music_cb_webpy.py : Recommend music to user based on cb,webpy,jieba

--- jieba_cb-music_webpy/music_cb_webpy.py
+++ jieba_cb-music_webpy/music_cb_webpy.py
@@ -6,6 +6,43 @@ import web
 import jieba
 import jieba.analyse
 
+
+words_dict = {}
+cur_word = 'NULL'
+
+with open('inverted_data.data', 'r') as fd:
+	for lines in fd:
+		ss = lines.strip().split('\t')
+		if len(ss) != 2:
+			continue
+		key_word = ss[0].strip()
+		infor = ss[1].strip()
+		
+		words_dict[key_word] = infor
+
+
+def word_in_dict(mywords_dict, words):
+	infor_list = []
+	for word in words:
+		if  word in mywords_dict.keys():
+			infor_list.append(mywords_dict[word])
+	
+	return infor_list
+
+
+def get_top3_infor(mylist):
+	tmp_list = []
+	for lt in mylist:
+		ss = lt.strip().split('')
+		ss = ss[:3]
+		for ww in ss:
+			w,s = ww.strip().split('')
+
+			tmp_list.append(('\t'.join([w, str(s)])))
+	
+	return tmp_list
+
+
 urls = (
 	'/' , 'index',
 	'/content', 'content',
@@ -24,16 +61,25 @@ class index:
 
 class content:
 	def GET(self):
-		my_word = []
+		my_list = []
+		tmp_lsit = []
 		par = web.input()
 		content = par.get('content', '')
 		words = jieba.analyse.extract_tags(content)
+		my_list = word_in_dict(words_dict,words)
+		tmp_list = get_top3_infor(my_list)
+
+		#print (tmp_list)
+		return "\n".join(tmp_list)
+
+		"""
 		for word in words:
 			print(word)
 			my_word.append(word)
 		
 		print (my_word)
 		return my_word
+		"""
 	
 if __name__ == "__main__":
 	app = web.application(urls, globals())
